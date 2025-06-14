622355beaa6dc35d921d04b7b85801f66d0286a7
EloAza <angelozeezag@gmail.com>
2025-06-14 13:31:20 +0700

    ubah transaksi list

--- app/Views/transaksi_list.php
+++ app/Views/transaksi_list.php
@@ -13,7 +13,7 @@
                     <th>Tanggal</th>
                     <th>Total Harga</th>
                     <th>Status</th>
-                </tr>
+                    <th>Tindakan</th> </tr>
             </thead>
             <tbody>
                 <?php foreach ($transaksi as $t): ?>
@@ -21,10 +21,40 @@
                         <td>#<?= $t['id_transaksi'] ?></td>
                         <td><?= date('d M Y H:i', strtotime($t['tanggal_transaksi'])) ?></td>
                         <td>Rp <?= number_format($t['total_harga_semua']) ?></td>
-                        <td><?= $t['status_pembayaran'] ?></td>
+                        <td>
+                            <span class="status <?= strtolower(str_replace(' ', '-', $t['status_pembayaran'])) ?>">
+                                <?= $t['status_pembayaran'] ?>
+                            </span>
+                        </td>
+                        <td>
+                            <?php if ($t['status_pembayaran'] == 'Belum Dibayar'): ?>
+                                <a href="<?= site_url('transaksi/konfirmasi/' . $t['id_transaksi']) ?>" class="btn btn-success" style="padding: 5px 10px; font-size: 0.9em;">
+                                    Konfirmasi Pembayaran
+                                </a>
+                            <?php else: ?>
+                                <span>✔ Lunas</span>
+                            <?php endif; ?>
+                        </td>
                     </tr>
                 <?php endforeach ?>
             </tbody>
         </table>
     <?php endif; ?>
+
+    <style>
+        .status {
+            padding: 3px 8px;
+            border-radius: 15px;
+            color: #fff;
+            font-size: 0.85em;
+            font-weight: bold;
+        }
+        .status.belum-dibayar {
+            background-color: #dc3545;
+        }
+        .status.lunas {
+            background-color: #28a745;
+        }
+    </style>
+
 <?= $this->endSection() ?>
